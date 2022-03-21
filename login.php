1dc8f948a786013e4379aa0050de9e181f18a7f0
as6991 <79053130+as6991@users.noreply.github.com>
2022-03-21 02:17:37 -0400

    Test

---
+++ login.php
@@ -0,0 +1,11 @@
+<?php
+function init () {
+if ( isset( $_POST['submit'] ) ) {
+        $firstname = $_REQUEST['user'];
+        $lastname = $_REQUEST['user_pass'];
+        echo 'Your name is ' . $lastname .' ' . $firstname;
+}
+}
+init();
+
+?>
