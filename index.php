fa01ef8e4dc122b201dd6911aca4ea84ef1e022f
as6991 <79053130+as6991@users.noreply.github.com>
2022-03-28 15:56:50 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php
-echo 'hi';
+
 if ( isset( $_POST['submit'] ) ) {
         $username = $_POST['user'];
         $password = $_POST['user_pass'];
+        if ($username == ' expensifytest@mailinator.com' && $password == 'hire_me'){
         echo 'Your name is ' . $username ;
         echo "<br>";
         echo  'Your password is ' . $password;
+        }
+        else {
+                echo 'Invalid Username or Password! Try Again!';
+        }
 }
 ?>
