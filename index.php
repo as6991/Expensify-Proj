7e4d413258ba79330ca2ac493426be653d6ca73f
as6991 <79053130+as6991@users.noreply.github.com>
2022-03-27 23:12:50 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 if ( isset( $_POST['submit'] ) ) {
         $username = $_POST['user'];
         $password = $_POST['user_pass'];
-        echo 'Your name is ' . $username .' ' . 'Your password is ' . $password;
+        echo 'Your name is ' . $username ;
+        echo  'Your password is ' . $password;
 }
 ?>
