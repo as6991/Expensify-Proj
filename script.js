8e32d24abf66e48d7b02c2001a74dcf2c3c03876
as6991 <79053130+as6991@users.noreply.github.com>
2022-05-04 23:38:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+fetch('POST https://www.expensify.com/api?command=Authenticate')
+    .then(res => {
+        if (res.ok) {
+            console.log("Success")
+            .then(res => res.json)
+        }   else {
+            console.log("Not Success")
+        }
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log('ERROR'))
