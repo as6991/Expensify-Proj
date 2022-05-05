e46a4537d77fd28e51586614e6716e1ec81a5553
as6991 <79053130+as6991@users.noreply.github.com>
2022-05-04 20:22:44 -0400

    revert

--- index.php
+++ index.php
@@ -3,13 +3,14 @@ if ( isset( $_POST['submit'] ) ) {
         $username = $_POST['user'];
         $password = $_POST['user_pass'];
         if (($username == 'expensifytest@mailinator.com' or $username == ' expensifytest@mailinator.com') && $password == 'hire_me'){               
-                        echo 'Your name is ' . $username ;
-                        echo "<br>";
-                        echo  'Your password is ' . $password;
+                        $res-> status = "Success";
+                        $myJSON = json_encode($res);
+                        echo $myJSON;
         }
         else {
                 echo 'Invalid Username or Password! Try Again!';
         }
 }
+
 ?>
 
