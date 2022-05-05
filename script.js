2dfe8d705917b9c6eca3d23b464213b254bcc7cd
as6991 <79053130+as6991@users.noreply.github.com>
2022-05-04 23:47:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-fetch('POST https://www.expensify.com/api?command=Authenticate')
-    .then(res => {
+fetch('https://www.expensify.com/api?command=Authenticate', {
+    method: 'POST', /**Used to create a new user */
+    headers: {   /** Changes formatting of code */                       
+        'Content-Type' : 'application/json'
+    },
+    body: JSON.stringify ({ /** Convert javascript to json string 
+                            this also is used to display data */
+        name:'User 1'
+    })
+})
+    .then(res => {      /** checks the response of the fetch */
         if (res.ok) {
             console.log("Success")
-            .then(res => res.json)
+            return res.json()
+
         }   else {
             console.log("Not Success")
         }
     })
-    .then(data => console.log(data))
+    .then(data => console.log(data))    /** displays the data */
     .catch(error => console.log('ERROR'))
