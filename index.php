bfdaca58a7225eb7b4c69f01e6af33b247229993
as6991 <79053130+as6991@users.noreply.github.com>
2022-05-07 00:52:05 -0400

    test

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
-if ( isset( $_POST['submit'] ) ) {
-        $username = $_POST['user'];
-        $password = $_POST['user_pass'];
-        if (($username == 'expensifytest@mailinator.com' or $username == ' expensifytest@mailinator.com') && $password == 'hire_me'){               
-                        $res-> status = "Success";
-                        $myJSON = json_encode($res);
-                        echo $myJSON;
-        }
-        else {
-                echo 'Invalid Username or Password! Try Again!';
-        }
-}
+// if ( isset( $_POST['submit'] ) ) {
+//         $username = $_POST['user'];
+//         $password = $_POST['user_pass'];
+//         if (($username == 'expensifytest@mailinator.com' or $username == ' expensifytest@mailinator.com') && $password == 'hire_me'){               
+//                         $res-> status = "Success";
+//                         $myJSON = json_encode($res);
+//                         echo $myJSON;
+//         }
+//         else {
+//                 echo 'Invalid Username or Password! Try Again!';
+//         }
+// }
+var_dump($_POST);
 
 ?>
 
--- script.js
+++ script.js
@@ -1,21 +1,40 @@
-fetch('https://www.expensify.com/api?command=Authenticate', {
-    method: 'POST', /**Used to create a new user */
-    headers: {   /** Changes formatting of code */                       
-        'Content-Type' : 'application/json'
-    },
-    body: JSON.stringify ({ /** Convert javascript to json string 
-                            this also is used to display data */
-        name:'User 1'
-    })
-})
-    .then(res => {      /** checks the response of the fetch */
-        if (res.ok) {
-            console.log("Success")
-            return res.json()
+//  fetch('https://www.expensify.com/api?command=Authenticate', {
+//     method: 'POST', /**Used to create a new user */
+//     headers: {   /** Changes formatting of code */                       
+//         'Content-Type' : 'application/json'
+//     },
+//     body: JSON.stringify ({ /** Convert javascript to json string 
+//                             this also is used to display data */
+//         name:'User 1'
+//     })
+// })
+//     .then(res => {      /** checks the response of the fetch */
+//         if (res.ok) {
+//             console.log("Success")
+//             return res.json()
 
-        }   else {
-            console.log("Not Success")
-        }
+//         }   else {
+//             console.log("Not Success")
+//         }
+//     })
+//     .then(data => console.log(data))    /** displays the data */
+//     .catch(error => console.log('ERROR')) 
+const myForm = document.getElementsById('myForm');
+
+myForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const formData = new FormData(this);
+    const searchParams =  new URLSearchParams();
+
+    fetch('index.php', {
+        method: 'post',
+        body: formData
+    }).then(function(response) {
+        return response.text();
+    }).then(function (text) {
+        console.log(text);
+    }).catch(function(error) {
+        console.error(error);
     })
-    .then(data => console.log(data))    /** displays the data */
-    .catch(error => console.log('ERROR'))
+});
